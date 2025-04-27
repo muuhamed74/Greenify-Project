9a3e141c872a3c91c6d713c54b83745dff539c7c
mohamed hesham <mohamedelhanafy290@gmail.com>
2025-04-27 14:49:14 +0300

    Fix connection error 990

--- Agricultural.Core/Models/PlantsInfo.cs
+++ Agricultural.Core/Models/PlantsInfo.cs
@@ -9,31 +9,26 @@ namespace Agricultural.Core.Models
 {
     public class PlantsInfo : BaseEntity
     {
-        [Required]
-        [MaxLength(100)]
+       
         public string PlantName { get; set; }
 
-        [Required]
-        [MaxLength(100)]
+        
         public string ScientificName { get; set; }
 
-        [Required]
-        [MaxLength(50)]
+       
         public string CareLevel { get; set; }
 
-        [Required]
-        [MaxLength(50)]
+       
         public string Size { get; set; }
 
-        [Required]
-        [MaxLength(50)]
+        
         public string Edibility { get; set; }
 
         public bool Flowering { get; set; }
         public bool Medicinal { get; set; }
         public bool IsAirPurifying { get; set; }
 
-        [Required]
+        
         public string About { get; set; }
 
         public PlantDetails Details { get; set; }
