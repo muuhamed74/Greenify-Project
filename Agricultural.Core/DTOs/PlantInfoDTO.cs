a1b491977661b8052d468046f22dae39dea3b6ef
mohamed hesham <mohamedelhanafy290@gmail.com>
2025-04-27 15:35:53 +0300

    Fix connection error 993

--- Agricultural.Core/DTOs/PlantInfoDTO.cs
+++ Agricultural.Core/DTOs/PlantInfoDTO.cs
@@ -2,20 +2,40 @@
 {
     public class PlantInfoDTO
     {
-        public string PlantName { get; set; }
-        public string SeasonType { get; set; }
-        public string SoilType { get; set; }
-        public double TemperatureMin { get; set; }
-        public double TemperatureMax { get; set; }
-        public double HumidityMin { get; set; }
-        public double HumidityMax { get; set; }
-        public string WaterNeeds { get; set; }
-        public string SunlightNeeds { get; set; }
-        public string GrowthTime { get; set; }
-        public string FertilizerType { get; set; }
-        public string CommonDiseases { get; set; }
-        public string PestControl { get; set; }
-        public string Uses { get; set; }
-        public List<PlantImageDTO> Images { get; set; }
+         public string PlantName { get; set; }
+
+        
+        public string ScientificName { get; set; }
+
+       
+        public string CareLevel { get; set; }
+
+       
+        public string Size { get; set; }
+
+        
+        public string Edibility { get; set; }
+
+        public bool Flowering { get; set; }
+        public bool Medicinal { get; set; }
+        public bool IsAirPurifying { get; set; }
+
+        
+        public string About { get; set; }
+
+        public PlantDetails Details { get; set; }
+
+     
     }
+
+    public class PlantDetails
+    {
+        public string Temperature { get; set; }
+        public string Sunlight { get; set; }
+        public string Water { get; set; }
+        public string Repotting { get; set; }
+        public string Fertilizing { get; set; }
+        public string Pests { get; set; }
+    }
+    
 }
